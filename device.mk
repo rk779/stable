5ec0c52f84fbf4ec0a737c602cabc2b39bcf9260
DennySPB <dennyspb@gmail.com>
2018-12-08 14:33:48 +0000

    s2: Kang WFD blobs from daisy
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>

--- device.mk
+++ device.mk
@@ -415,6 +415,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wfd
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # SDcard
 PRODUCT_CHARACTERISTICS := nosdcard
 
